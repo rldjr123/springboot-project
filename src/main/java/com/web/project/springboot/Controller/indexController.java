3d2ba8a301fe8dd405f78807922cc13fcb7a587f
Yunjung <tkznfk1402@naver.com>
2021-08-31 00:39:04 +0900

    2021/08/31  - 반영내역    1. Repository 및 Contoller and Service 수정    2. index.mustache 수정 및 조회확인

--- src/main/java/com/web/project/springboot/Controller/indexController.java
+++ src/main/java/com/web/project/springboot/Controller/indexController.java
@@ -1,13 +1,22 @@
 package com.web.project.springboot.Controller;
 
+import com.web.project.springboot.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
 
+    private final PostsService postsService;
+
+    public indexController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
--- src/main/java/com/web/project/springboot/posts/PostsService.java
+++ src/main/java/com/web/project/springboot/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.web.project.springboot.posts;
 
 import com.web.project.springboot.domain.posts.Posts;
 import com.web.project.springboot.domain.posts.PostsRepository;
+import com.web.project.springboot.dto.PostsListResponseDto;
 import com.web.project.springboot.dto.PostsResponseDto;
 import com.web.project.springboot.dto.PostsSaveRequestDto;
 import com.web.project.springboot.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +32,19 @@ public class PostsService {
         return id;
     }
 
-
     public PostsResponseDto findById(long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+
 
 }
