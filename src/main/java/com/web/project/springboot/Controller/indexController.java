44057bb81e03204430a710241648dbd9e902a530
Yunjung <tkznfk1402@naver.com>
2021-09-01 00:10:56 +0900

    2021/09/01  - 반영내역    1. 게시글 update / delete 작성

--- src/main/java/com/web/project/springboot/Controller/indexController.java
+++ src/main/java/com/web/project/springboot/Controller/indexController.java
@@ -1,9 +1,11 @@
 package com.web.project.springboot.Controller;
 
+import com.web.project.springboot.dto.PostsResponseDto;
 import com.web.project.springboot.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
@@ -21,8 +23,15 @@ public class indexController {
     }
 
     @GetMapping("/posts/save")
-    public String postssave(){
+    public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+        return "posts-update";
     }
 
 }
