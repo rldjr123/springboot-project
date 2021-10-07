45a135135ef5cac8f7bde730367790bd6e801095
Yunjung <tkznfk1402@naver.com>
2021-10-07 21:49:49 +0900

    - 2021-10-07    1. Profile 수정

--- src/main/java/com/web/project/springboot/Controller/ProfileController.java
+++ src/main/java/com/web/project/springboot/Controller/ProfileController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real","real1","real2");
-        String defaultProfile = profiles.isEmpty()? "default": profiles.get(0);
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
                 .orElse(defaultProfile);
     }
-
 }
