a0aa52dadf6a5bab657c5da96309f6d7c7a37da2
Yunjung <tkznfk1402@naver.com>
2021-10-11 17:57:19 +0900

    - 2021-10-11    1. script 파일수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readling -f $0)
+ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-project
+PROJECT_NAME=freelec-springboot2-webservice
 
-echo ">Build 파일복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 어플리케이션배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> Jar Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -25,8 +25,8 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로실행합니다"
-nohub java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,home/ec2-user/app.application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
